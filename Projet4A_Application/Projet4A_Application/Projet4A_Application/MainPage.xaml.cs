d7ccafd4144dd34dbca3058e0f87bb388b6468b9
WyderSevrin <sevrin.wyder@gmail.com>
2020-12-21 20:00:42 +0100

    First Commit

---
+++ Projet4A_Application/Projet4A_Application/Projet4A_Application/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Projet4A_Application
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
